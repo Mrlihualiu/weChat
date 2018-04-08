db54662f0acf1dfbe2699f5bdf9ed6fd1d12b525
mrlihualiu <hc_liulihua@163.com>
2018-04-08 10:29:55 +0800

    1

--- firstDemo/pages/list/list.js
+++ firstDemo/pages/list/list.js
@@ -1,181 +1,165 @@
-// pages/list/list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-
-  data: {
-    metto:"原来的样子",
-    imgUrls:[
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000,
-    x:0,
-    y:0,
-    loading:true,
-    button_type:'warn',
-    button_text:'加载中...',
-    disabled:true,
-    checkbox_group_data:[
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
-      { name: 'JPN', value: '日本' },
-      { name: 'ENG', value: '英国' },
-      { name: 'TUR', value: '法国' },
-    ],
-    checkbox_arr:'',
-    focus: false,
-    inputValue: ''
-  },
-  tap:function(){
-    this.setData({
-      x:30,
-      y:30
-    });
-  },
-  changeIndicatorDots:function(e){
-    this.setData({
-      indicatorDots:!this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    console.log(e)
-    this.setData({
-      interval: e.detail.value
-    });
-  },
-  durationChange: function (e) {
-    console.log(e)
-    this.setData({
-      duration: e.detail.value
-    })
-  },
-  //button 按钮
-  changeLoading:function(){
-    if(this.data.button_type === 'warn'){
-      this.setData({
-        button_type: 'primary',
-        button_text: '成功'
-      })
-    }else{
-      this.setData({
-        button_type:'warn',
-        button_text:'加载中...'
-      })
+    data: {
+        array: ['美国', '中国', '巴西', '日本'],
+        objectArray: [
+            {
+                id: 0,
+                name: '美国'
+            },
+            {
+                id: 1,
+                name: '中国'
+            },
+            {
+                id: 2,
+                name: '巴西'
+            },
+            {
+                id: 3,
+                name: '日本'
+            }
+        ],
+        index: 0,
+        multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+        objectMultiArray: [
+            [
+                {
+                    id: 0,
+                    name: '无脊柱动物'
+                },
+                {
+                    id: 1,
+                    name: '脊柱动物'
+                }
+            ], [
+                {
+                    id: 0,
+                    name: '扁性动物'
+                },
+                {
+                    id: 1,
+                    name: '线形动物'
+                },
+                {
+                    id: 2,
+                    name: '环节动物'
+                },
+                {
+                    id: 3,
+                    name: '软体动物'
+                },
+                {
+                    id: 3,
+                    name: '节肢动物'
+                }
+            ], [
+                {
+                    id: 0,
+                    name: '猪肉绦虫'
+                },
+                {
+                    id: 1,
+                    name: '吸血虫'
+                }
+            ]
+        ],
+        multiIndex: [0, 0, 0],
+        date: '2016-09-01',
+        time: '12:01',
+        region: ['广东省', '广州市', '海珠区'],
+        customItem: '全部'
+    },
+    bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
+    },
+    bindMultiPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            multiIndex: e.detail.value
+        })
+    },
+    bindMultiPickerColumnChange: function (e) {
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+        var data = {
+            multiArray: this.data.multiArray,
+            multiIndex: this.data.multiIndex
+        };
+        data.multiIndex[e.detail.column] = e.detail.value;
+        switch (e.detail.column) {
+            case 0:
+                switch (data.multiIndex[0]) {
+                    case 0:
+                        data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+                        data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                        break;
+                    case 1:
+                        data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+                        data.multiArray[2] = ['鲫鱼', '带鱼'];
+                        break;
+                }
+                data.multiIndex[1] = 0;
+                data.multiIndex[2] = 0;
+                break;
+            case 1:
+                switch (data.multiIndex[0]) {
+                    case 0:
+                        switch (data.multiIndex[1]) {
+                            case 0:
+                                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                                break;
+                            case 1:
+                                data.multiArray[2] = ['蛔虫'];
+                                break;
+                            case 2:
+                                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                                break;
+                            case 3:
+                                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                                break;
+                            case 4:
+                                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                                break;
+                        }
+                        break;
+                    case 1:
+                        switch (data.multiIndex[1]) {
+                            case 0:
+                                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                                break;
+                            case 1:
+                                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                                break;
+                            case 2:
+                                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                                break;
+                        }
+                        break;
+                }
+                data.multiIndex[2] = 0;
+                console.log(data.multiIndex);
+                break;
+        }
+        this.setData(data);
+    },
+    bindDateChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            date: e.detail.value
+        })
+    },
+    bindTimeChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            time: e.detail.value
+        })
+    },
+    bindRegionChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            region: e.detail.value
+        })
     }
-    this.setData({
-      loading:!this.data.loading,
-      disabled:!this.data.disabled
-    })
-  },
-  //checkbox-group 单选框
-  checkboxChange:function(e){
-    this.setData({
-      checkbox_arr:e.detail.value
-    })
-  },
-  //from 表单
-  formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-  },
-  formReset: function () {
-    console.log('form发生了reset事件')
-  },
-  //input 输入框
-  bindButtonTap: function () {
-    this.setData({
-      focus: true
-    })
-  },
-  bindKeyInput: function (e) {
-    this.setData({
-      inputValue: e.detail.value
-    })
-  },
-  bindReplaceInput: function (e) {
-    var value = e.detail.value
-    var pos = e.detail.cursor
-    if (pos != -1) {
-      //光标在中间
-      var left = e.detail.value.slice(0, pos)
-      //计算光标的位置
-      pos = left.replace(/11/g, '2').length
-    }
-
-    //直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
-    return {
-      value: value.replace(/11/g, '2'),
-      cursor: pos
-    }
-
-    //或者直接返回字符串,光标在最后边
-    //return value.replace(/11/g,'2'),
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
